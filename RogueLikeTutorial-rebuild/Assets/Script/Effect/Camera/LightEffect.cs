cda8acb5263106d63669b8ffdfcdce17307f08e8
リン <linjerry0906@gmail.com>
2018-11-09 00:42:14 +0900

    [add] light map 仮実装

---
+++ RogueLikeTutorial-rebuild/Assets/Script/Effect/Camera/LightEffect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightEffect : MonoBehaviour 
+{
+	[SerializeField] private Camera lightMapCamera;
+	[SerializeField] private Material lightMapMaterial;
+	private RenderTexture renderTexture;
+
+	private void Awake() 
+	{
+		renderTexture = RenderTexture.GetTemporary (Screen.width, Screen.height);
+		renderTexture.wrapMode = TextureWrapMode.Clamp;
+		lightMapCamera.targetTexture = renderTexture;
+		lightMapMaterial.SetFloat("_resoX", Screen.currentResolution.width);
+		lightMapMaterial.SetFloat("_resoY", Screen.currentResolution.height);
+	}
+	
+	void OnRenderImage(RenderTexture src,RenderTexture dest)
+    {
+        lightMapMaterial.SetTexture ("_LightMap", renderTexture);
+        Graphics.Blit (src, dest, lightMapMaterial);
+    }
+}
