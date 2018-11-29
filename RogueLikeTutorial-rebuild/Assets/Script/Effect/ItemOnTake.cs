2f8f04dbca6c1214a8fa05f73b7a0dc8a02ebf0c
リン <linjerry0906@gmail.com>
2018-11-29 09:41:55 +0900

    [update] Item作成

---
+++ RogueLikeTutorial-rebuild/Assets/Script/Effect/ItemOnTake.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemOnTake : MonoBehaviour 
+{
+	[SerializeField] private ParticleSystem particle;
+
+	public void OnTake(Transform taker)
+	{
+		particle.transform.parent = taker;
+		particle.transform.localPosition = Vector3.zero;
+		particle.Play();
+	}
+}
